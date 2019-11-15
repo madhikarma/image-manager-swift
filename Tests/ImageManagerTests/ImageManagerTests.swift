ba5907bbd92b41a85474356592fe8d6acead0363
Shagun Madhikarmi <madhikarma@gmail.com>
2019-11-15 06:58:17 +0000

    Fixes tests for image loading

--- Tests/ImageManagerTests/ImageManagerTests.swift
+++ Tests/ImageManagerTests/ImageManagerTests.swift
@@ -2,8 +2,8 @@ import XCTest
 @testable import ImageManager
 
 final class ImageManagerTests: XCTestCase {
-    func testExample() {        
-        ImageManager().loadImage(for: URL(string: "https://mages.app.goo.gl/ZUfB5Zbp5JhZ4isz5")!) { (result) in
+    func testLoadImage() {
+        ImageManager().loadImage(for: URL(string: "https://cdn1.itpro.co.uk/sites/itpro/files/styles/article_main_wide_image/public/2015/12/swift-og.png?itok=YSTgCI6L")!) { (result) in
             guard let _ = try? result.get() else {
                 XCTFail("Error: expected image to load successfully")
                 return
@@ -11,8 +11,19 @@ final class ImageManagerTests: XCTestCase {
             XCTAssertTrue(true)
         }
     }
+    
+    func testLoadImageFailure() {
+        ImageManager().loadImage(for: URL(string: "https://dn1.itpro.co.uk/sites/itpro/files/styles/article_main_wide_image/public/2015/12/swift-og.png?itok=YSTgCI6L")!) { (result) in
+            if let _ = try? result.get() {
+                XCTFail("Error: expected no imgae")
+                return
+            }
+            XCTAssertTrue(true)
+        }
+    }
+
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testLoadImage", testLoadImage),
     ]
 }
